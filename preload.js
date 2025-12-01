0acb081a5ee7ac955cd975134719bb7d8ef771d6
ddt907640411 <ddt907640411@gmail.com>
2025-12-01 23:07:57 +0800

    Update preload.js
    const { contextBridge, ipcRenderer } = require('electron');
    
    contextBridge.exposeInMainWorld('api', {
        addCustomer: (n, p) => ipcRenderer.invoke('add-customer', n, p),
        searchCustomer: (kw) => ipcRenderer.invoke('search-customer', kw),
        addOrder: (cid, item, price) => ipcRenderer.invoke('add-order', cid, item, price),
        getOrders: () => ipcRenderer.invoke('get-orders')
    });

--- preload.js
+++ preload.js
@@ -1,1 +1,9 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+    addCustomer: (n, p) => ipcRenderer.invoke('add-customer', n, p),
+    searchCustomer: (kw) => ipcRenderer.invoke('search-customer', kw),
+    addOrder: (cid, item, price) => ipcRenderer.invoke('add-order', cid, item, price),
+    getOrders: () => ipcRenderer.invoke('get-orders')
+});
 
